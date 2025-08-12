8be3eddc29f2a52d1062028bf410623b4952cbfd
Chirag <chiragsuvarna95@gmail.com>
2025-08-12 23:10:41 +0530

    Created Service class, need to complete it.

--- EmployeeManagementSystem/src/main/java/com/project/EmployeeManagement/services/EmployeeService.java
+++ EmployeeManagementSystem/src/main/java/com/project/EmployeeManagement/services/EmployeeService.java
@@ -1,5 +1,39 @@
 package com.project.EmployeeManagement.services;
 
+import com.project.EmployeeManagement.entities.Employee;
+import com.project.EmployeeManagement.dao.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class EmployeeService {
 
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeService.class);
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        logger.info("Adding employee: {}", employee.getEmail() );
+        return employeeRepository.save(employee);
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        logger.info("Updating employee with id: {}", employee.getId());
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployeeById(Long employeeId) {
+        logger.info("Deleting employee with id: {}", employeeId);
+        employeeRepository.deleteById(employeeId);
+    }
+
+   
 }
+
