145b239138712bfffe2a2e86879568276c63d7af
Chirag <chiragsuvarna95@gmail.com>
2025-08-13 15:13:14 +0530

    Controller not working, getting 401 error

--- EmployeeManagementSystem/src/main/java/com/project/EmployeeManagement/controller/EmployeeController.java
+++ EmployeeManagementSystem/src/main/java/com/project/EmployeeManagement/controller/EmployeeController.java
@@ -1,9 +1,13 @@
 package com.project.EmployeeManagement.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.EmployeeManagement.entities.Employee;
@@ -11,16 +15,23 @@ import com.project.EmployeeManagement.services.EmployeeServiceImpl;
 
 @RestController
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeServiceImpl employeeServiceImpl;
-	
+
 	@GetMapping("/employees")
-	public List<Employee> getAllEmployees()
-	{
+	public List<Employee> getAllEmployees() {
 		return this.employeeServiceImpl.getAllEmployees();
 	}
-	
-	
+
+	@GetMapping("/employee/{s_id}")
+	public Optional<Employee> getEmployeeById(@PathVariable("s_id") Long id) {
+		return this.employeeServiceImpl.getEmployeeById(id);
+	}
+
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee emp) {
+		return employeeServiceImpl.addEmployee(emp);
+	}
 
 }
