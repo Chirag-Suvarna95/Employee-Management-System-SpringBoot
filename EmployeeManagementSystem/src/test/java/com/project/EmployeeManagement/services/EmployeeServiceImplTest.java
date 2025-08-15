de96c7b873e6a37acdb1d1f812212ecda291bb8d
Chirag <chiragsuvarna95@gmail.com>
2025-08-15 18:06:07 +0530

    Test class for Service

---
+++ EmployeeManagementSystem/src/test/java/com/project/EmployeeManagement/services/EmployeeServiceImplTest.java
@@ -0,0 +1,48 @@
+package com.project.EmployeeManagement.services;
+
+import com.project.EmployeeManagement.dao.EmployeeRepository;
+import com.project.EmployeeManagement.entities.Employee;
+import com.project.EmployeeManagement.exceptions.EmailAlreadyExistsException;
+import com.project.EmployeeManagement.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class EmployeeServiceImplTest {
+
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    @InjectMocks
+    private EmployeeServiceImpl employeeService;
+
+    private Employee testEmployee;
+
+    @BeforeEach
+    void setUp() {
+        testEmployee = new Employee();
+        testEmployee.setId(1L);
+        testEmployee.setFirstName("John");
+        testEmployee.setLastName("Doe");
+        testEmployee.setEmail("john.doe@example.com");
+        testEmployee.setDepartment("IT");
+        testEmployee.setJobTitle("Developer");
+        testEmployee.setSalary(75000.0);
+    }
+
+
+}
